d9b43c48af510bbb88a474ebc7e6531cecc0fda0
j.parker.garrison@gmail.com <j.parker.garrison@gmail.com>
2019-03-23 00:19:13 -0400

    changed columns

--- src/data/DataCsvInterface.py
+++ src/data/DataCsvInterface.py
@@ -19,20 +19,31 @@ class DataCsvInterface:
         CSV_FILE_NAME:    The name of the csv file. Can be a preprocessed version instead of original.
         DEPENDENT_NAME:   The y column to predict
         CATEGORY_NAMES:   The columns to handle categorically
-        CONTINUOUS_NAMES: the columns to handled as continuous. If None, the non-cat, non-dep cols become con
+        CONTINUOUS_NAMES: the columns to handled as continuous. If None, the non-cat, non-dep cols become continuous
     """
     ABS_CSV_PATH = str(Path(__file__).parents[0])
     CSV_FILE_NAME = "foreveralone_shuf.csv"
     DEPENDENT_NAME = "attempt_suicide"
-    CATEGORY_NAMES = ['gender', 'sexuallity', 'race', 'virgin', 'prostitution_legal', 'pay_for_sex', 'social_fear',
-                      'depressed', 'what_help_from_others', 'employment', 'edu_level', 'improve_yourself_how','income',  'bodyweight', ]
-    CONTINUOUS_NAMES = ['age', 'friends']
+    CATEGORY_NAMES = [e+"_categorical" for e in ['gender', 'sexuallity', 'race', 'virgin', 'prostitution_legal', 'pay_for_sex', 'social_fear', 'depressed', 'employment', 'edu_level', 'bodyweight', 'income']]
+    CONTINUOUS_NAMES = ['age', 'income_float', 'friends']
+    NF1_NAMES = ['what_help_from_others', 'improve_yourself_how']
+
+    TRASH_NAMES = ['time', 'job_title']
+
+    NF1_CONTINUOUS_NAMES = []
+    NF1_CATEGORICAL_NAMES = ['improve_yourself_how', 'what_help_from_others']
+
+    CATEGORY_NAMES += ['improve_yourself_how_'+str(i) for i in range(1,53+1)]
+
+    CATEGORY_NAMES += ['improve_yourself_how_' + str(i) for i in range(1, 53 + 1)]
+    CATEGORY_NAMES += ['what_help_from_others' + str(i) for i in range(1, 45 + 1)]
+
+
 
     # needs to become continuous edu_level
     # need to preproc income avgerage on both sides of " to ", bodyweight
     # what_help_from_others, improve_yourself_how is 0nf
     # employment should be split
-    NF1_NAMES = ['what_help_from_others', 'improve_yourself_how']
     VARIABLE_NAMES = ['income', 'bodyweight', 'prostitution_legal', 'pay_for_sex', 'friends', 'social_fear',
                       'depressed', 'employment', 'job_title', 'edu_level', 'what_help_from_others',
                       'improve_yourself_how', 'virgin']
